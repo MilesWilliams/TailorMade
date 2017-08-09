99c4f44ad8c6b9e5b458c771e6c1a4977f26cedd
MilesWilliams <miles@klarity.co.za>
2017-08-09 18:00:43 +0200

    Added railway font and signup / login buttons

--- Tailor Made/UIViewExtensions.swift
+++ Tailor Made/UIViewExtensions.swift
@@ -7,6 +7,11 @@ extension UIView {
             self.alpha = 1.0
         }, completion: completion)  }
     
+    func fadeInHalf(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 0.5
+        }, completion: completion)  }
+    
     func fadeOut(_ duration: TimeInterval = 1.0, delay: TimeInterval = 0.0, completion: @escaping (Bool) -> Void = {(finished: Bool) -> Void in}) {
         UIView.animate(withDuration: duration, delay: delay, options: UIViewAnimationOptions.curveEaseIn, animations: {
             self.alpha = 0.0
--- Tailor Made/ViewController.swift
+++ Tailor Made/ViewController.swift
@@ -11,16 +11,41 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var background: UIImageView!
     @IBOutlet var welcomeTextLabel: UITextField!
+    @IBOutlet var loginBTN: UIButton!
 
+    @IBOutlet var signupBTN: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+//        let gold = UIColor(red: 178/255, green: 152/255, blue: 93/255, alpha: 1).cgColor
+        let goldOpaque = UIColor(red: 178/255, green: 152/255, blue: 93/255, alpha: 0.4).cgColor
+        self.welcomeTextLabel.alpha = 0
+        self.welcomeTextLabel.font = UIFont(name: "Raleway-Thin", size: 20)
+        self.loginBTN.titleLabel?.font = UIFont(name: "Raleway-Thin", size: 16)
+        self.signupBTN.titleLabel?.font = UIFont(name: "Raleway-Thin", size: 16)!
+        self.welcomeTextLabel.text = "Welcome..."
+        self.loginBTN.alpha = 0
+        self.loginBTN.layer.borderWidth = 1
+        self.loginBTN.layer.borderColor = goldOpaque
+        self.loginBTN.layer.backgroundColor = goldOpaque
+        self.signupBTN.alpha = 0
+        self.signupBTN.layer.borderWidth = 1
+        self.signupBTN.layer.borderColor = goldOpaque
+
+
     }
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        enterButton()
+        
+        self.welcomeTextLabel.fadeIn(delay:0.5)
+        self.welcomeTextLabel.fadeOut(delay: 1, completion: {
+            (finished: Bool) -> Void in
+            self.enterButton()
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,17 +54,9 @@ class ViewController: UIViewController {
     }
     
     func enterButton() {
-        self.welcomeTextLabel.alpha = 0
-        self.welcomeTextLabel.fadeOut(completion: {
-            (finished: Bool) -> Void in
-            self.welcomeTextLabel.text = "Welcome..."
-            self.welcomeTextLabel.fadeIn()
-            self.welcomeTextLabel.fadeOut(completion: {
-                (finished: Bool) -> Void in
-                self.welcomeTextLabel.text = "Enter"
-                self.welcomeTextLabel.fadeIn()
-            })
-        })
+
+        self.loginBTN.fadeIn()
+        self.signupBTN.fadeIn()
 
     }
     
